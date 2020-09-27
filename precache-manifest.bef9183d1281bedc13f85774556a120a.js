84db9fbd95d2c56fdb88e02ee91ab2646dc01137
Sebastian Seitz <bassistsagainstracists@gmx.net>
2020-09-27 13:21:36 +0200

    Updates

---
+++ precache-manifest.bef9183d1281bedc13f85774556a120a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65340dc541965eea9f1602de9534e9d3",
+    "url": "/cl4assmusik/index.html"
+  },
+  {
+    "revision": "1506602953394f8ace59",
+    "url": "/cl4assmusik/static/css/main.ddd6d31d.chunk.css"
+  },
+  {
+    "revision": "8b0a2c0fdd0e59d17985",
+    "url": "/cl4assmusik/static/js/2.63417fc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cl4assmusik/static/js/2.63417fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1506602953394f8ace59",
+    "url": "/cl4assmusik/static/js/main.a2e60a6d.chunk.js"
+  },
+  {
+    "revision": "dc472a1142acf4cab4ab",
+    "url": "/cl4assmusik/static/js/runtime-main.26ef06d1.js"
+  }
+]);
